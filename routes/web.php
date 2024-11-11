7862eaad180426a8b6cd28e64fc7e9bdf9bc50fa
Gibekkk <gfoucauld@student.ciputra.ac.id>
2024-11-11 15:33:57 +0800

    Revert "upload"
    This reverts commit f4e255ee67ff2c78c0428c51bdfe69d447845aab.

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/CV', function () {
-    return view('cv');
-});
-Route::get('/projects', function () {
-    return view('projects');
-});
